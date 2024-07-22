e4328ceb0cf6cba31bfb64b0d1d4e1d438b85fb0
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-07-22 18:55:56 +0530

    Part 3 - JwtService added

--- src/main/java/com/example/demo4/SecurityApp/entities/User.java
+++ src/main/java/com/example/demo4/SecurityApp/entities/User.java
@@ -1,13 +1,21 @@
 package com.example.demo4.SecurityApp.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements UserDetails {
 
     @Id
