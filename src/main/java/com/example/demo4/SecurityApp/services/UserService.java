91541f2ca74728fb4bd3047e360953292ea57ba1
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-07-22 16:04:45 +0530

    Part 1 - Spring security dependency added, User Entity created

---
+++ src/main/java/com/example/demo4/SecurityApp/services/UserService.java
@@ -0,0 +1,22 @@
+package com.example.demo4.SecurityApp.services;
+
+import com.example.demo4.SecurityApp.exceptions.ResourceNotFoundException;
+import com.example.demo4.SecurityApp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User with email "+ username +" not found"));
+    }
+}
