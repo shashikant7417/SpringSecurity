470c618b344937b45ea816240964e787c6bb1d9c
Anuj Kumar Sharma <anuj55149@gmail.com>
2024-07-31 19:41:28 +0530

    Part 9 - Session Management added

--- src/main/java/com/example/demo4/SecurityApp/services/AuthService.java
+++ src/main/java/com/example/demo4/SecurityApp/services/AuthService.java
@@ -16,6 +16,7 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserService userService;
+    private final SessionService sessionService;
 
     public LoginResponseDto login(LoginDto loginDto) {
         Authentication authentication = authenticationManager.authenticate(
@@ -25,12 +26,14 @@ public class AuthService {
         User user = (User) authentication.getPrincipal();
         String accessToken = jwtService.generateAccessToken(user);
         String refreshToken = jwtService.generateRefreshToken(user);
+        sessionService.generateNewSession(user, refreshToken);
 
         return new LoginResponseDto(user.getId(), accessToken, refreshToken);
     }
 
     public LoginResponseDto refreshToken(String refreshToken) {
         Long userId = jwtService.getUserIdFromToken(refreshToken);
+        sessionService.validateSession(refreshToken);
         User user = userService.getUserById(userId);
 
         String accessToken = jwtService.generateAccessToken(user);
